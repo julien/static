5f24909aa481a9421f813be00194fd5d1e5a3a2d
Julien Castelain <julien.castelain@liferay.com>
2019-10-05 22:47:24 +0200

    Be a little bit more careful

--- static.sh
+++ static.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm *.html
+rm -i *.html
 for i in *.md; do
 	n=$(basename "$i" | sed s/\.md/.html/)
 	echo "<a href=\"$n\">$(echo "$n" | sed "s/\.html//" | sed "s/\_/ /")</a><br>" >> index.html
