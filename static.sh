814c4e0c1d149fe4030cd9ba1e102bc0175db05d
Julien Castelain <julien@users.noreply.github.com>
2019-12-20 00:03:03 +0100

    Update static.sh

--- static.sh
+++ static.sh
@@ -2,6 +2,6 @@
 rm -i *.html
 for i in *.md; do
 	n=$(basename "$i" | sed s/\.md/.html/)
-	echo "<a href=\"$n\">$(echo "$n" | sed "s/\.html//" | sed "s/\_/ /")</a><br>" >> index.html
+	echo "<a href=\"$n\">$(echo "$n" | sed -e "s/\.html//" -e "s/\_/ /")</a><br>" >> index.html
 	Markdown.pl "$i" > "$n"
 done
