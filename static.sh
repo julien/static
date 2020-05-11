5da64f4c987bd83165f101cbae470a20a4314d91
Julien Castelain <julien.castelain@liferay.com>
2020-05-11 22:42:25 +0200

    Add some html around markdown output
    shellcheck is not happy about this, but I'll fix it later

--- static.sh
+++ static.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
-rm -i *.html
-for i in *.md; do
-	n=$(basename "$i" | sed s/\.md/.html/)
-	echo "<a href=\"$n\">$(echo "$n" | sed -e "s/\.html//" -e "s/\_/ /")</a><br>" >> index.html
-	Markdown.pl "$i" > "$n"
-done
+set -o errexit
+set -o pipefail
+
+__main() {
+	#rm -i *.html
+	for i in $(find . -name '*.md' -not -iname 'readme*'); do
+		n=$(basename "$i" | sed s/\.md/.html/)
+		echo "<a href=\"$n\">$(echo "$n" | sed -e "s/\.html//" -e "s/\_/ /")</a><br>" >> index.html
+		#Markdown.pl "$i" > "$n"
+		#echo "$i"
+		local md
+		local output
+		md=$(Markdown.pl "$i")
+		output=$(cat <<-END
+		<!doctype html>
+		<html>
+			<body>
+				$md
+			</body>
+		</html>
+		END
+		)
+		echo "$output" >> "$n"
+	done
+}
+
+__main "$@"
