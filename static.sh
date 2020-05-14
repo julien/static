5207d31314fe551efb64efa912dfe7147bf3361f
Julien Castelain <julien.castelain@liferay.com>
2020-05-14 21:24:06 +0200

    Just add the required html, no need for more

--- static.sh
+++ static.sh
@@ -3,25 +3,23 @@ set -o errexit
 set -o pipefail
 
 __main() {
-	#rm -i *.html
+	rm -i *.html
+
 	for i in $(find . -name '*.md' -not -iname 'readme*'); do
 		n=$(basename "$i" | sed s/\.md/.html/)
 		echo "<a href=\"$n\">$(echo "$n" | sed -e "s/\.html//" -e "s/\_/ /")</a><br>" >> index.html
-		#Markdown.pl "$i" > "$n"
-		#echo "$i"
+
 		local md
-		local output
 		md=$(Markdown.pl "$i")
+
+		local output
 		output=$(cat <<-END
-		<!doctype html>
-		<html>
-			<body>
-				$md
-			</body>
-		</html>
+			<meta name=viewport content="width=device-width, initial-scale=1">
+			$md
 		END
 		)
-		echo "$output" >> "$n"
+
+		echo "$output" > "$n"
 	done
 }
 
