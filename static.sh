d55a8b4eeb8da5761ee81736c8b7f42c8df28714
Julien Castelain <julien.castelain@liferay.com>
2020-05-15 15:39:33 +0200

    A monster is born

--- static.sh
+++ static.sh
@@ -3,24 +3,55 @@ set -o errexit
 set -o pipefail
 
 __main() {
-	rm -i *.html
+	[[ -f "./index.html" ]] && rm -i "./index.html"
 
-	for i in $(find . -name '*.md' -not -iname 'readme*'); do
-		n=$(basename "$i" | sed s/\.md/.html/)
-		echo "<a href=\"$n\">$(echo "$n" | sed -e "s/\.html//" -e "s/\_/ /")</a><br>" >> index.html
+	local list=""
 
-		local md
-		md=$(Markdown.pl "$i")
-
+	while IFS= read -r -d '' file
+	do
 		local output
-		output=$(cat <<-END
-			<meta name=viewport content="width=device-width, initial-scale=1">
-			$md
+		output=$(basename "$file" | sed -e s/\.md/.html/ -e s/[[:blank:]]/-/g)
+
+		local date
+		date=$(date -r "$file" -I)
+
+		list+=$(cat <<-END
+			<li><a href="$output">$(echo "$output" | sed -e "s/\.html//" -e "s/-/ /") <time datetime="$date" style="float: right;font-size: smaller">$date</time></a></li>
 		END
 		)
 
-		echo "$output" > "$n"
-	done
+	local html
+	html=$(cat <<-END
+<meta name=viewport content=width=device-width,initial-scale=1>
+$(Markdown.pl "$file")
+END
+)
+		echo "$html" > "$output"
+
+	done < <(find . -name '*.md' -not -iname 'readme*' -print0)
+
+	local index
+	index=$(cat <<-END
+			<!DOCTYPE html>
+			<html>
+			<head>
+				<meta name=viewport content="width=device-width, initial-scale=1">
+				<style>
+					ul {margin:0;padding:0}
+					li {margin: 8px 0;list-style:none;}
+					time {float:right;font-size:small;}
+				</style>
+			</head>
+			<body>
+				<ul>
+					$list
+				</ul>
+			</body>
+			</html>
+		END
+	)
+	echo "$index" >> index.html
+	echo "Generated"
 }
 
 __main "$@"
